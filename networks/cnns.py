- updated transformer model parameters v3 - fixed issue in training.py related to pic output size from environment - normalized score/reward from environment to fix issue with large gradients

networks/cnns.py
@@ -299,7 +299,7 @@ class CNNExtractor(nn.Module):
                 self.save_output_image(x, i)
         return x
 
-    def save_output_image(self, tensor: torch.Tensor, layer_index: int, save_every_x_images: int = 250000) -> None:
+    def save_output_image(self, tensor: torch.Tensor, layer_index: int, save_every_x_images: int = 10) -> None:
         """
         Saves the output images of a convolutional layer for a batch of samples.
 