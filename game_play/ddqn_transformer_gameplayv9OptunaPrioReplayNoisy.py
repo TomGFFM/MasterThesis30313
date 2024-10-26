0215737a3f4d3683d6297c3cf3587767e79a04f9
TomGFFM <thomas.graf@icloud.com>
2024-10-26 16:53:56 +0200

    - Final change in parameter spaces to allow broader training

--- game_play/ddqn_transformer_gameplayv9OptunaPrioReplayNoisy.py
+++ game_play/ddqn_transformer_gameplayv9OptunaPrioReplayNoisy.py
@@ -56,8 +56,8 @@ env = gym.make("ALE/SpaceInvaders-v5", render_mode="human", frameskip=1)
 # #####################################################
 # ################ init hyperparameter ################
 # #####################################################
-agent_params_file = open('/Users/thomas/Repositories/MasterThesis30313/output_remote/metrics/trial_3_DDQAugmentedNoisyTransformerNN_agent_hyper_params.yaml', 'r')
-network_params_file = open('/Users/thomas/Repositories/MasterThesis30313/output_remote/metrics/trial_3_DDQAugmentedNoisyTransformerNN_network_hyper_params.yaml', 'r')
+agent_params_file = open('/Users/thomas/Repositories/MasterThesis30313/output_remote/metrics/trial_7_DDQAugmentedNoisyTransformerNN_agent_hyper_params.yaml', 'r')
+network_params_file = open('/Users/thomas/Repositories/MasterThesis30313/output_remote/metrics/trial_7_DDQAugmentedNoisyTransformerNN_network_hyper_params.yaml', 'r')
 agent_hyper_params = yaml.load(agent_params_file, Loader=yaml.FullLoader)
 network_hyper_params = yaml.load(network_params_file, Loader=yaml.FullLoader)
 
@@ -110,7 +110,7 @@ trained_agent = DeepQNetworkAgentPrioritizedNoisy(policy_net=policy_net,
                                                   loss_function=loss_function)
 
 # load pre-trained model into agent
-trained_agent.load('/Users/thomas/Repositories/MasterThesis30313/output_remote/models/20241007_trial_3_DDQAugmentedNoisyTransformerNN_episode_563_score_0.36.pth', map_location=device)
+trained_agent.load('/Users/thomas/Repositories/MasterThesis30313/output_remote/models/20241024_trial_7_DDQAugmentedNoisyTransformerNN_final_model.pth', map_location=device)
 
 output_size = network_hyper_params['input_shape'][1]
 
